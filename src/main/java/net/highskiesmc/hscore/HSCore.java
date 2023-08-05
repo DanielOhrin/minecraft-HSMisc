884860cc844b82f72d370d5277f45e66eb18fc12
Daniel Ohrin <xqiam@outlook.com>
2023-08-05 05:42:09 -0500

    Configurable Creeper Health with RS

--- src/main/java/net/highskiesmc/hscore/HSCore.java
+++ src/main/java/net/highskiesmc/hscore/HSCore.java
@@ -5,7 +5,9 @@ import com.mattisadev.mcore.highskies.HSPlugin;
 import net.highskiesmc.hscore.commands.commands.HSCoreCommand;
 import net.highskiesmc.hscore.commands.tabcompleters.HSCoreTabCompleter;
 import net.highskiesmc.hscore.events.handlers.ExplosionHandler;
+import net.highskiesmc.hscore.events.handlers.CreeperHealthHandler;
 import net.highskiesmc.hscore.events.handlers.TogglePvpHandler;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import javax.annotation.Nonnull;
@@ -13,6 +15,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public final class HSCore extends HSPlugin {
+    public static final boolean USING_ROSESTACKER = Bukkit.getPluginManager().getPlugin("RoseStacker") != null;
+    private CreeperHealthHandler creeperHealthHandler;
     @Override
     public void enable() {
         getCommand("hscore").setExecutor(new HSCoreCommand(this));
@@ -20,6 +24,11 @@ public final class HSCore extends HSPlugin {
 
         register(new ExplosionHandler(this));
         register(new TogglePvpHandler(this));
+
+        if (USING_ROSESTACKER) {
+            this.creeperHealthHandler = new CreeperHealthHandler(this);
+            register(this.creeperHealthHandler);
+        }
     }
 
     @Override
@@ -29,7 +38,8 @@ public final class HSCore extends HSPlugin {
 
     @Override
     public void reload() {
-
+        if (USING_ROSESTACKER)
+            this.creeperHealthHandler.setMaxHealth(getConfig().getInt("spawner.creepah.spawn-health", 10));
     }
 
     @Override
---
+++ src/main/java/net/highskiesmc/hscore/events/handlers/CreeperHealthHandler.java
@@ -0,0 +1,69 @@
+package net.highskiesmc.hscore.events.handlers;
+
+import dev.rosewood.rosestacker.RoseStacker;
+import dev.rosewood.rosestacker.event.AsyncEntityDeathEvent;
+import dev.rosewood.rosestacker.event.EntityStackEvent;
+import dev.rosewood.rosestacker.event.EntityStackMultipleDeathEvent;
+import dev.rosewood.rosestacker.event.EntityUnstackEvent;
+import dev.rosewood.rosestacker.manager.StackManager;
+import dev.rosewood.rosestacker.stack.StackedEntity;
+import net.highskiesmc.hscore.HSCore;
+import org.bukkit.entity.Creeper;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.SpawnerSpawnEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class CreeperHealthHandler implements Listener {
+    private final HSCore MAIN;
+    private final StackManager stackManager;
+    private int maxHealth;
+
+    public void setMaxHealth(int hp) {
+        this.maxHealth = hp;
+    }
+
+    public CreeperHealthHandler(HSCore main) {
+        this.MAIN = main;
+        this.stackManager = RoseStacker.getInstance().getManager(StackManager.class);
+        this.maxHealth = main.getConfig().getInt("spawner.creepah.spawn-health", 10);
+    }
+
+    @EventHandler
+    public void onSpawnerSpawn(SpawnerSpawnEvent e) {
+        if (e.getEntity() instanceof Creeper creepah)
+            creepah.setHealth(maxHealth);
+    }
+
+    @EventHandler
+    public void onMobsDoThings(EntityStackEvent e) {
+        if (e.getStack().getEntity() instanceof Creeper creepah)
+            creepah.setHealth(maxHealth);
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent e) {
+        if (!(e instanceof AsyncEntityDeathEvent) && e.getEntity() instanceof Creeper creepah)
+            handleCreeperStackHealth(e, creepah);
+    }
+
+    private void handleCreeperStackHealth(EntityDeathEvent e, Creeper creepah) {
+        if (this.stackManager.isWorldDisabled(creepah.getWorld()))
+            return;
+
+        if (!this.stackManager.isEntityStackingEnabled())
+            return;
+
+        StackedEntity stackedEntity = this.stackManager.getStackedEntity(creepah);
+        if (stackedEntity == null)
+            return;
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                stackedEntity.getEntity().setHealth(maxHealth);
+            }
+        }.runTaskLater(this.MAIN, 1);
+    }
+}
